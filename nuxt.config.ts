354a85a9577fadc3e0b02e3901410e654c7efada
msoler75 <msoler75@yahoo.es>
2023-03-19 09:07:11 -0300

    ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
 
+  modules: [
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
+    'nuxt-icon'
+  ],
 
-modules: [
-  '@pinia/nuxt',
-  '@pinia-plugin-persistedstate/nuxt',
-  '@nuxtjs/tailwindcss',
-  '@nuxtjs/color-mode',
-  'nuxt-icon',
-],
-
-    app: {
-        head: {
-          script: [{ src: 'main.prod.js' }],
-        },
-      }
-    
-
-    })
+  app: {
+    head: {
+      script: [{ src: 'main.prod.js' }]
+    }
+  }
+})
